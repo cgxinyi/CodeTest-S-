3dc124ca4e3ed388305fe085db6c1cbff14aa59c
cgxinyi <xygan3@student.monash.edu>
2021-08-18 15:35:00 +1000

    hide token

--- springbootcdk/lib/fargate.ts
+++ springbootcdk/lib/fargate.ts
@@ -49,7 +49,7 @@ export class SpringbootfagateStack extends cdk.Stack {
           
           actionName: 'GitHub',
           output: sourceArtifact,
-          oauthToken: cdk.SecretValue.plainText('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'),
+          oauthToken: SecretValue.secretsManager('github-token'),
           // Replace these with your actual GitHub project info
           owner: 'cgxinyi',
           repo: 'CodeTest-Seis',
